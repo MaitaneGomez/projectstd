72b7e7c0d8b15dc9ce75c3974aa6b7ce5c6856d4
zuri.otx1988 <zuri.otx1988@828d4619-7179-b80d-158b-718d4a772f6b>
2010-04-28 08:03:25 +0000

    con el servidor empezado

--- trunk/EURECS/src/environment/server/EnvironmentServer.java
+++ trunk/EURECS/src/environment/server/EnvironmentServer.java
@@ -3,6 +3,8 @@ package environment.server;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
+import bd.DBManager;
+
 import environment.util.SocketManager;
 
 
@@ -13,6 +15,9 @@ public class EnvironmentServer
 	private SocketManager sm;
 	private int state;
 	private String user;
+	private String password;
+	private String listsensor;
+	private DBManager DB;
 	
 	public EnvironmentServer(SocketManager sManager) throws Exception 
 	{
@@ -26,65 +31,108 @@ public class EnvironmentServer
 		try 
 		{
 			String clientSentence = sm.Leer();
-			//System.out.println("The client says: " + clientSentence);
+			System.out.println("The client says: " + clientSentence);
 			
 			StringTokenizer token = new StringTokenizer(clientSentence);
 			String command = token.nextToken();
 			//Cuidado con los tokens, si no hay nada da error, excepcion
 			
-			
-			if (command.equalsIgnoreCase("QUIT"))
+			while(state!=4)
 			{
-				state=4;
-				sm.Escribir("208 OK Bye");
-			}
-			else 
-			{
-				switch (state)
+				if (command.equalsIgnoreCase("QUIT"))
 				{
-					case 0:
+					state=4;
+					sm.Escribir("208 OK Bye");
+				}
+				else 
+				{
+					switch (state)
 					{
-						if(command.equalsIgnoreCase("USER"))
+						case 0:
 						{
-							user = token.nextToken();
-							//Ahora miramos en la BD si existe el user 
-							//si es que si pues sacamos "201 OK Welcome Mikel"
-							// si no existe en la bd "401 ERR Missing username parameter"							
+							if(command.equalsIgnoreCase("USER"))
+							{
+								
+								//Ahora miramos en la BD si existe el user 
+								//si es que si pues sacamos "201 OK Welcome Mikel"
+								// si no existe en la bd "401 ERR Missing username parameter"
+								try
+								{
+									user = token.nextToken();
+									if( DB.checkUser(user))
+									{
+										sm.Escribir("201 OK Welcome " + user);
+										state=1;
+									}
+									else sm.Escribir("400 Username does not exist");
+								}
+								catch(Exception e)
+								{	
+									sm.Escribir("401 ERR Missing username parameter ");
+								}
+									
+							} 
+							break;
 						}
-						break;
-					}
-					
-					case 1:
-					{
-					
-						break;
-					}
-					
-					
-					case 2:
-					{
-					
-						break;
-					}
-					
-					case 3:
-					{
+							
+						case 1:
+						{
+							if(command.equalsIgnoreCase("PASS"))
+							{
+								try
+								{
+									password = token.nextToken();
+									if( DB.checkPass(user,password))
+									{
+										sm.Escribir("201 OK Welcome to the system" );
+										state=2;
+									}
+									else 
+									{
+										sm.Escribir("402 ERR Authentication error");
+										state=0;
+									}
+									//Ahora miramos en la BD si existe el user 
+								}
+								catch(Exception e)
+								{
+									sm.Escribir("403 ERR Missing password parameter");
+								}
+							}
+							break;
+						}
 						
-						break;
+						
+						case 2:
+						{
+							if(command.equalsIgnoreCase("LISTSENSOR"))
+							{
+								
+							}
+						
+							break;
+						}
+						
+						case 3:
+						{
+							
+							break;
+						}
 					}
 				}
 			}
+		}
+				
+	
+				
+				
 			
-			
-			
-			
-		} 
 		catch (IOException e) 
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		
 
 	}
